82e82c69cd89a4589d3630332fcf730650d2a51a
Areeb Gondal <areeb.gondal1@gmail.com>
2023-01-05 15:35:26 +0500

    added states

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import {NativeBaseProvider, Box, Stack, Input, Button} from 'native-base';
-
+import React, {useState} from 'react';
+import {NativeBaseProvider, Box, Stack, Input, Button, Text} from 'native-base';
 export default function App() {
+  const [Data, setData] = useState<any>('');
+  const [Data1, setData1] = useState<any>('');
+
   return (
     <NativeBaseProvider>
       <Stack flex={1} bgColor={'blueGray.200'}>
         <Input
+          testID="1"
           variant={'rounded'}
           borderWidth={'4'}
           borderColor={'black'}
-          mt={'12'}></Input>
-        <Button variant={'solid'} borderRadius={'2xl'} mt={'12'}>
-          submit
+          mt={'12'}
+          value={Data}
+          onChangeText={setData}></Input>
+        <Button
+          variant={'solid'}
+          borderRadius={'2xl'}
+          mt={'12'}
+          onPress={() => setData('')}>
+          clear
         </Button>
+        <Text>{Data}</Text>
       </Stack>
     </NativeBaseProvider>
   );
