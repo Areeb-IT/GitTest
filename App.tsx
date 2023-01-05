b107a624e144bf22f6b42dae5ab7dc8c1ecce2b1
Areeb Gondal <areeb.gondal1@gmail.com>
2023-01-05 15:10:04 +0500

    user interface init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import {NativeBaseProvider, Box} from 'native-base';
+import {NativeBaseProvider, Box, Stack, Input, Button} from 'native-base';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <Stack flex={1} bgColor={'blueGray.200'}>
+        <Input
+          variant={'rounded'}
+          borderWidth={'4'}
+          borderColor={'black'}
+          mt={'12'}></Input>
+        <Button variant={'solid'} borderRadius={'2xl'} mt={'12'}>
+          submit
+        </Button>
+      </Stack>
     </NativeBaseProvider>
   );
 }
